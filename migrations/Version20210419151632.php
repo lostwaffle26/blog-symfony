8c8286d86605fdfbf81d10d6e1d79d8b1089ca14
Tom <tomruiz2611@gmail.com>
2021-04-20 13:43:33 +0200

    fix fixtures

---
+++ migrations/Version20210419151632.php
@@ -0,0 +1,41 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20210419151632 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE articles CHANGE slug slug VARCHAR(128) NOT NULL');
+        $this->addSql('CREATE UNIQUE INDEX UNIQ_BFDD3168989D9B62 ON articles (slug)');
+        $this->addSql('ALTER TABLE categories CHANGE slug slug VARCHAR(128) NOT NULL');
+        $this->addSql('CREATE UNIQUE INDEX UNIQ_3AF34668989D9B62 ON categories (slug)');
+        $this->addSql('ALTER TABLE key_words CHANGE slug slug VARCHAR(128) NOT NULL');
+        $this->addSql('CREATE UNIQUE INDEX UNIQ_93436343989D9B62 ON key_words (slug)');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP INDEX UNIQ_BFDD3168989D9B62 ON articles');
+        $this->addSql('ALTER TABLE articles CHANGE slug slug VARCHAR(255) CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_unicode_ci`');
+        $this->addSql('DROP INDEX UNIQ_3AF34668989D9B62 ON categories');
+        $this->addSql('ALTER TABLE categories CHANGE slug slug VARCHAR(60) CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_unicode_ci`');
+        $this->addSql('DROP INDEX UNIQ_93436343989D9B62 ON key_words');
+        $this->addSql('ALTER TABLE key_words CHANGE slug slug VARCHAR(60) CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_unicode_ci`');
+    }
+}
--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,6 +2,11 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Articles;
+use App\Entity\Users;
+use App\Entity\KeyWords;
+use App\Entity\Commentary;
+use App\Entity\Categories;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
@@ -9,9 +14,36 @@ class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
-        // $product = new Product();
-        // $manager->persist($product);
-
+        // Articles
+        $articles = new Articles();
+        $manager->persist($articles);
         $manager->flush();
+      
+        // Users 
+        $users = new Users();
+        $users->setEmail('admin@admin.com');
+        $users->setRoles(array('ROLE_ADMIN'));
+
+        $password = $this->encoder->encodePassword($users, 'adminadmin');
+        $users->setPassword($password);
+
+        $manager->persist($users);
+        $manager->flush();
+
+        // Key words
+        $keyWords = new KeyWords();
+        $manager->persist($keyWords);
+        $manager->flush();
+
+        // Commentaires
+        $commentary = new Commentary();
+        $manager->persist($commentary);
+        $manager->flush();
+
+        // Catégories
+        $categories = new Categories();
+        $manager->persist($categories);
+        $manager->flush();
+
     }
 }
